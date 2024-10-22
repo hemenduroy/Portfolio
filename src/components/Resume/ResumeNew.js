2d6849d17038219f1b84ca37e99e562dedfd9989
Hemendu Roy <hroy6@asu.edu>
2024-10-22 15:56:14 -0700

    unify resumelink

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "https://raw.githubusercontent.com/hemendu/portfolio/master/src/Assets/resume.pdf";
+const resumeLink = "/resume.pdf"
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -23,7 +21,7 @@ function ResumeNew() {
       <Container fluid className="resume-section">
         <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
+          <Button variant="primary" href={resumeLink} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
@@ -36,7 +34,7 @@ function ResumeNew() {
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button variant="primary" href={pdf} target="_blank">
+          <Button variant="primary" href={resumeLink} target="_blank">
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
